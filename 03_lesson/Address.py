2e9fad831b73aaa36140f8164b661dc717e8875c
Katerina Gubanova <k-gubanova@list.ru>
2026-02-05 02:22:49 +0300

    homework3

---
+++ 03_lesson/Address.py
@@ -0,0 +1,12 @@
+class Address:
+    def __init__(self, zip_code, city, street, house, apartment):
+        self.zip = zip_code
+        self.city = city
+        self.street = street
+        self.house = house
+        self.apartment = apartment
+
+   # def __str__(self):
+    #    return f"{self.zip} {self.city} {self.street} {self.house} {self.apartment}"
+
+
---
+++ 03_lesson/lesson_3_task_1.py
@@ -0,0 +1,7 @@
+from user import User
+
+my_user = User("Katya", "Gubanova")
+
+print(my_user.sayfirstname())
+print(my_user.saylastname())
+print(my_user.sayfullname())
